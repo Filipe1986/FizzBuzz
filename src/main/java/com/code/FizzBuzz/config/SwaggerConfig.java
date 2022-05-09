dafcb2999ac0670c52dcd72a908556f4cec41062
filipe <filipe.esteves.goncalves@gmail.com>
2022-05-09 15:50:50 -0300

    Making the swagger config more simple

--- src/main/java/com/code/FizzBuzz/config/SwaggerConfig.java
+++ src/main/java/com/code/FizzBuzz/config/SwaggerConfig.java
@@ -27,9 +27,7 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
-
-		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+		registry.addResourceHandler("swagger-ui.html").addResourceLocations("");
 	}
 
 }
